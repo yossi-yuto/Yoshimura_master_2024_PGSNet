7029bfa67555f23a0fb9200e97849621ddd199ef
yossi-yuto <yoshimura@g.wakayama-u.jp>
2025-01-16 10:18:39 +0900

    readable shell scripts

--- train_infer.sh
+++ train_infer.sh
@@ -13,12 +13,35 @@ fi
 
 # ループ処理
 for i in "sph" "tray" "cup" "planes" "ash"; do  
+
     echo "proposed training on ${i}"
-    CUDA_VISIBLE_DEVICES=${device_id} python train.py -dataset_path ${dataset_path}  -mirror_type ${i} -result_dir ${result_dir}/proposed/${i} -batch_size 2 -random_seed ${RANDOM_SEED}
+    CUDA_VISIBLE_DEVICES=${device_id} python train.py\
+        -dataset_path ${dataset_path}\
+        -mirror_type ${i}\
+        -result_dir ${result_dir}/proposed/${i}\
+        -batch_size 2\
+        -random_seed ${RANDOM_SEED}\
+        -patient 15
+
     echo "proposed inference on ${i}"
-    CUDA_VISIBLE_DEVICES=${device_id} python infer.py -dataset_path ${dataset_path} -mirror_type ${i} -result_dir ${result_dir}/proposed/${i} 
+    CUDA_VISIBLE_DEVICES=${device_id} python infer.py\
+        -dataset_path ${dataset_path}\
+        -mirror_type ${i}\
+        -result_dir ${result_dir}/proposed/${i}
+
     echo "baseline training on ${i}"
-    CUDA_VISIBLE_DEVICES=${device_id} python train_pgsnet.py -dataset_path ${dataset_path}  -mirror_type ${i} -result_dir ${result_dir}/VMD/${i} -batch_size 2 -random_seed ${RANDOM_SEED} -model pgsnet
+    CUDA_VISIBLE_DEVICES=${device_id} python train_pgsnet.py\
+    -dataset_path ${dataset_path}\
+    -mirror_type ${i}\
+    -result_dir ${result_dir}/VMD/${i}\
+    -batch_size 2\
+    -random_seed ${RANDOM_SEED}\
+    -model pgsnet
+
     echo "baseline inference on ${i}"
-    CUDA_VISIBLE_DEVICES=${device_id} python infer_pgsnet.py -dataset_path ${dataset_path} -mirror_type ${i} -result_dir ${result_dir}/VMD/${i} -model pgsnet
+    CUDA_VISIBLE_DEVICES=${device_id} python infer_pgsnet.py\
+    -dataset_path ${dataset_path}\
+    -mirror_type ${i}\
+    -result_dir ${result_dir}/VMD/${i}\
+    -model pgsnet
 done
