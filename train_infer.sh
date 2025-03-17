b321023d4a27f2bb5b1d8476819f9f5c7be1b797
yossi-yuto <yoshimura@g.wakayama-u.jp>
2025-03-18 01:18:14 +0900

    revised: remove comment out

--- train_infer.sh
+++ train_infer.sh
@@ -1,47 +1,63 @@
 #!/bin/bash
 device_id=$1
 result_dir="results/$2"
-dataset_path="/data2/yoshimura/mirror_detection/DATA/videos"
-RANDOM_SEED=2024
-result_dir=${result_dir}_seed${RANDOM_SEED}
+dataset_path="./videos"
+RANDOM_SEEDS=(42)
 
-# 引数が渡されていない場合のエラーチェック
 if [ -z "$device_id" ]; then
     echo "Usage: $0 <device_id>"
     exit 1
 fi
 
-# ループ処理
-for i in "sph" "tray" "cup" "planes" "ash"; do  
+for RANDOM_SEED in ${RANDOM_SEEDS[@]}; do
+    echo "Random Seed" ${RANDOM_SEED}
+    curr_result_dir=${result_dir}_seed${RANDOM_SEED}
 
-    echo "proposed training on ${i}"
-    CUDA_VISIBLE_DEVICES=${device_id} python train.py\
+    for i in "sph" "tray" "cup" "planes" "ash"; do  
+        echo "baseline training on ${i}"
+        CUDA_VISIBLE_DEVICES=${device_id} python train_pgsnet.py\
         -dataset_path ${dataset_path}\
         -mirror_type ${i}\
-        -result_dir ${result_dir}/proposed/${i}\
+        -result_dir ${curr_result_dir}/PGS/${i}\
         -batch_size 2\
         -random_seed ${RANDOM_SEED}\
-        -patient 15
+        -model pgsnet
 
-    echo "proposed inference on ${i}"
-    CUDA_VISIBLE_DEVICES=${device_id} python infer.py\
+        echo "baseline inference on ${i}"
+        CUDA_VISIBLE_DEVICES=${device_id} python infer_pgsnet.py\
         -dataset_path ${dataset_path}\
         -mirror_type ${i}\
-        -result_dir ${result_dir}/proposed/${i}
+        -result_dir ${curr_result_dir}/PGS/${i}\
+        -random_seed ${RANDOM_SEED}\
+        -model pgsnet
 
-    echo "baseline training on ${i}"
-    CUDA_VISIBLE_DEVICES=${device_id} python train_pgsnet.py\
-    -dataset_path ${dataset_path}\
-    -mirror_type ${i}\
-    -result_dir ${result_dir}/VMD/${i}\
-    -batch_size 2\
-    -random_seed ${RANDOM_SEED}\
-    -model pgsnet
+        echo "proposed phase 1"
+        CUDA_VISIBLE_DEVICES=${device_id} python train_proposed.py\
+            -dataset_path ${dataset_path}\
+            -mirror_type ${i}\
+            -result_dir ${result_dir}/proposed/${i}\
+            -batch_size 2\
+            -random_seed ${RANDOM_SEED}\
+            -patient 10\
+            -phase 1
 
-    echo "baseline inference on ${i}"
-    CUDA_VISIBLE_DEVICES=${device_id} python infer_pgsnet.py\
-    -dataset_path ${dataset_path}\
-    -mirror_type ${i}\
-    -result_dir ${result_dir}/VMD/${i}\
-    -model pgsnet
+        echo "proposed phase 2"
+        CUDA_VISIBLE_DEVICES=${device_id} python train_proposed.py\
+            -dataset_path ${dataset_path}\
+            -mirror_type ${i}\
+            -result_dir ${curr_result_dir}/proposed/${i}\
+            -batch_size 2\
+            -random_seed ${RANDOM_SEED}\
+            -patient 15\
+            -phase 2
+
+        echo "proposed inference on ${i}"
+        CUDA_VISIBLE_DEVICES=${device_id} python infer_proposed.py\
+            -dataset_path ${dataset_path}\
+            -mirror_type ${i}\
+            -result_dir ${curr_result_dir}/proposed/${i}\
+            -random_seed ${RANDOM_SEED}\
+            --test_only
+
+    done
 done
